95d826f87f9a1568ab47a3f5a0d71fe270b24f60
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-08-03 17:31:25 -0300

    Primeiro arquivo de VUE JS
    Aula 31/07/2023

---
+++ aula-31-07-2023/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="container">
+<h1>Hello world</h1>
+<h2> {{ nome }}</h2>
+<h3>{{ usuario.idade }}</h3>
+<input type="text" v-model="nome">
+</div>
+</template>
+
+<script>
+export default {
+data(){
+  return{
+    nome: 'Eduardo Phelipe',
+    usuario: {
+      idade: 33,
+      email: 'eduardo@gmail.com'
+    }
+  }
+}
+}
+
+</script>
+
+<style>
+.container {
+  width: 100vw;
+  height: 100vh;
+  background-color: rgb(121, 53, 211);
+}
+</style>
