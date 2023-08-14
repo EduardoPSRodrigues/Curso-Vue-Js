a401c80294a1035fd730ba401bfe7021bb897000
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-08-14 14:33:14 -0300

    Criação do Components - Card

---
+++ aula-02-08-2023-components/src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <slot name="header"></slot>
+    </header>
+
+    <slot></slot>
+
+    <footer>
+        <slot name="footer"></slot>
+
+    </footer>
+</template>
+   
+<script>
+</script>
+   
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
+
+<!-- Informações do projeto
+
+* <slot></slot> serve para marcar onde será exibido aquele dado que no caso essa informação esta no App.vue em uma tag p
+
+
+
+-->
