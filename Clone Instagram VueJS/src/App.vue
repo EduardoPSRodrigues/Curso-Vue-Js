0b55a55e76670a989272cbb4cac668d5d5e748f0
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-08-17 20:09:40 -0300

    Atualizando o arquivo

--- Clone Instagram VueJS/src/App.vue
+++ Clone Instagram VueJS/src/App.vue
@@ -1,29 +1,11 @@
 <!-- HTML -->
 <template>
 
-<router-view>
-
-</router-view>
-
+<router-view> </router-view>
 
 </template>
 
-
-<!-- Java Script -->
-
-<script>
-</script>
-
-
-<!-- Estilização -->
- 
-<style >
-  
-</style>
-
 <!-- Informações do projeto
 * Primeiro passo criar todas as telas da aplicação.
-
-
 
 -->
