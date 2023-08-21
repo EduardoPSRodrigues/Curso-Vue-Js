40861baf7d7dba05181af637200673d858a828e8
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-08-21 20:07:00 -0300

    Funcionamento do AXIOS com o mounted

---
+++ Aula-15-08-2023/Exercicio Pratico Axios/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+ <!-- <form>
+    <h1>Axios via GitHub</h1>
+    <h2>Nome: {{ usuario.name }}</h2>
+     <img :src="usuario.avatar_url" :width="200" alt="imagem"> 
+  </form>-->
+
+  <form>
+    <h1>Axios via Via Cep</h1>
+    <h2>Endereço: {{ endereco.logradouro }}</h2>
+    <h2>CEP: {{ endereco.cep }}</h2>
+    <h2>complemento: {{ endereco.complemento }}</h2>
+    <h2>bairro: {{ endereco.bairro }}</h2>
+    <h2>localidade: {{ endereco.localidade }}</h2>
+    
+  </form>
+</template>
+
+<script>
+/**
+ * PASSOS PARA UTILIZAR O AXIOS
+ * 
+ * Não precisa fazer a validação e a conversão para json, sendo que no fetch tem que fazer na mão
+ * Não precisa fazer a configuração do tipo de conteúdo que esta passando
+ * 
+ * 
+ * 1 - [x] instalar o axios - npm install axios
+ * 2 - [x] importar o axios no local que será utilizado - import axios from "axios"
+ * 3 - [x] utilizar o axios usando o método (GET, POST, PUT...) desejado
+ * 
+ * mounted(){ } é responsável por montar o site, entao se estou pegando valores de outros lugares para depois montar o meu site
+ * o site sempre ficará atualizado, 
+ */
+
+import axios from "axios"
+
+export default {
+  data(){
+    return {
+     usuario: {},
+     endereco: {},
+    }
+  },
+  // mounted(){
+  //   axios.get('https://api.github.com/users/Bruno-Costa-fig')
+  //   .then(resultado => this.usuario = resultado.data)
+  //   .catch(erro => console.log('erro'))
+  // }, 
+  mounted() {
+    axios.get('https://viacep.com.br/ws/01001000/json/')
+    .then(resultado => this.endereco = resultado.data)
+    .catch(erro => console.log('erro'))
+  },
+}
+</script>
+
+<style scoped>
+form{
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+</style>
