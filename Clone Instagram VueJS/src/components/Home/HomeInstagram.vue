37fa58121d64cbea39d2950de82e5c40870e4d87
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-08-22 18:09:15 -0300

    Atualizando a tela Home

--- Clone Instagram VueJS/src/components/Home/HomeInstagram.vue
+++ Clone Instagram VueJS/src/components/Home/HomeInstagram.vue
@@ -1,7 +1,23 @@
 <template>
-    <div>
-        <p>Home</p>
-    </div>
+
+ <div class="content" id="conteudo">
+            <h1>Conteúdo principal</h1>
+            <p>Este é o conteúdo principal da página</p>
+
+            <p id="bem-vindo-usuario"></p>
+
+            <p>Hora Atual: <span id="hora-Atual"> </span></p>
+            <p>Nome: <span id="nome-Usuario"> </span></p>
+            <p>Username: <span id="User-Name"> </span></p>
+            <p>Quantidade de Seguidores: <span id="qtd-de-seguidores"> </span></p>
+            <p>Quantidade de Publicações: <span id="qtd-de-publicacoes"> </span></p>
+            <p>Quantidade de Pessoas Seguindo: <span id="qtd-de-pessoas-seguindo"> </span></p>
+
+            <div id="galeria" class="galeria-fotos">
+            </div>
+
+
+        </div>
 </template>
 
 
@@ -12,7 +28,14 @@ export default {
 </script>
 
 
-<style>
+<style scoped>
+
+.content{
+    flex: 1;
+    padding: 20px;
+    background-color: #fafafa;
+    margin-left: 200px;
+}
     
 </style>
 
