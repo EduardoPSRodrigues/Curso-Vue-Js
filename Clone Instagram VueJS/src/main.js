f831799b90e3e1c7c76fc25084b825e4714aa3f1
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-08-22 18:09:06 -0300

    Importando e configurando o Vuetify

--- Clone Instagram VueJS/src/main.js
+++ Clone Instagram VueJS/src/main.js
@@ -4,6 +4,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes/routes.js'
 
+// Vuetify 
+import 'vuetify/styles' 
+import '@mdi/font/css/materialdesignicons.css' 
+import { createVuetify } from 'vuetify' 
+import * as components from 'vuetify/components' 
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({ components, directives, icons: { defaultSet: 'mdi' }, })
+
 createApp(App)
 .use(router)
+.use(vuetify)
 .mount('#app')
