0c4c3792df4210e64d0cdc40a918060fbfea0fbd
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-08-22 17:54:28 -0300

    Usando o Vuetify

---
+++ vuetify-instalando-biblioteca/src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <h1>Home</h1>
+    
+    <v-btn append-icon="mdi-check-bold" color="green" variant="tonal">
+      Button
+    </v-btn>
+
+    <v-card class="container"  title="Poema do Manolo" subtitle="Categoria: Coragem" variant="tonal"
+      text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias ex pariatur dolores incidunt quo 
+      optio fugiat nulla quaerat magni ipsam, animi nihil reprehenderit repellendus tenetur aliquid doloribus, ducimus iste rerum!">
+
+      <v-card-actions>
+        <v-btn prepend-icon="mdi-thumb-up-outline">Curtir</v-btn>
+      </v-card-actions>
+    </v-card>
+    
+  </main>
+</template>
+
+<script >
+
+</script>
+
+<style scoped>
+.container{
+display: flex;
+flex-direction: column;
+gap: 10px;
+width: 50%;
+}
+</style>
