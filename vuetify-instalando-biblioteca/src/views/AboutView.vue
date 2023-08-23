eb230a9a00d2ce9721fffe0a008bcccb9f0a5f40
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-08-23 11:24:07 -0300

    Atualização do arquivo com comentários

--- vuetify-instalando-biblioteca/src/views/AboutView.vue
+++ vuetify-instalando-biblioteca/src/views/AboutView.vue
@@ -28,6 +28,7 @@
     <v-btn variant="tonal" color="green" type="submit">Cadastrar</v-btn>
     <v-btn variant="tonal" color="orange" type="reset">Limpar</v-btn>
   </v-form>
+
   <v-snackbar
     v-model="snackbar"
     :timeout="timeout"
@@ -73,7 +74,7 @@ export default {
       if(valid){ 
         this.text = 'Usuário cadastrado com sucesso!'
         this.snackbar = true
-        this.$refs.form.reset()
+        this.$refs.form.reset() //Faz a limpeza do formulário, o nome do meu formulário é form
       }
     }
   }
@@ -83,3 +84,21 @@ export default {
 <style>
 
 </style>
+
+<!-- Informações do projeto
+1 -  ref="form" é como se fosse o getElementById, pois eu estou colocando uma referência em uma tag
+2 - É a maneira como o Vuetify trabalha, const {valid} = await this.$refs.form.validate(), esta basicamente falando para
+    ir na referencia form e por estar usando o $, ele me permite usar todas as propriedades, então estou usando a propriedade
+    de validação e estou falando para ver se a const [valid] é true ou false, se valid for verdadeiro eu faço o cadastro.
+    Ou seja, bloqueio o botão para só ficar ativado quando tudo for preenchido
+    o comando async faz com que o programa execute a função em segundo plano
+    e o comando await significa que o código ficará travado até que seja validado
+3 - :timeout="timeout" é quantos milesegundos vai demorar para ser disparado, normalmente são 2000 = 2 segundos
+4 - this.text = 'Usuário cadastrado com sucesso!' é a mensagem que vai aparecer no "alert" do Vuetify 
+    this.snackbar = true o botão esta em data como false, ao clicar na função, ele mudará o status para true
+5 - Ao colocar um botão com o type reset dentro do form, ao clicar no botão, ele limpa os campos dos inputs sem precisar configurar
+    <v-btn variant="tonal" color="orange" type="reset">Limpar</v-btn>
+
+
+
+-->
