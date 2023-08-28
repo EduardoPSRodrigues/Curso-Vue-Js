d882a0bdaf8d12a4aea5ceba70fe3c764b68ff72
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-08-28 15:32:41 -0300

    Estilizando o Home com o Vuetify

--- Clone Instagram VueJS/src/components/Home/HomeInstagram.vue
+++ Clone Instagram VueJS/src/components/Home/HomeInstagram.vue
@@ -1,65 +1,80 @@
 <template>
-    <h1>Conteúdo principal</h1>
-    <p>Este é o conteúdo principal da página.</p>
-    <p id="bem_vindo_usuario"></p>
-    <p>Nome: <span id="nome"></span> </p>
-    <p>Username: <span id="username"></span> </p>
-    <p>Qtd de seguidores: <span id="qtd_seguidores"></span> </p>
-    <p>Qtd de publicações: <span id="qtd_publicacoes"></span> </p>
 
-    <v-form class="d-flex" ref="form" @submit.prevent="handleSubmit">
-        <v-text-field 
-            class="w-25 me-5"
-            label="Nome" 
-            variant="outlined" 
-            :rules="[v => !!v || 'Nome é obrigatório!']"
-            v-model="post.usuario"
-            required
-        ></v-text-field>
+    <v-layout>
+        <v-app-bar color="blue" >
+            <v-app-bar-title> Instagram</v-app-bar-title>
 
-        <v-text-field 
-            class="w-50"
-            label="Imagem" 
-            variant="outlined" 
-            :rules="[v => !!v || 'Imagem é obrigatória!']"
-            v-model="post.imagem"
-            required
-        ></v-text-field>
+            <v-btn icon>
+                <router-link to="/posts/novo" >
+            <v-icon>mdi-magnify</v-icon>
+                </router-link>
+            </v-btn>
+        </v-app-bar>
 
-        <v-btn variant="tonal" class="mx-5" color="green" type="submit">Cadastrar</v-btn>
-        <v-btn variant="tonal" color="orange" type="reset">Limpar</v-btn>
-    </v-form>
+    <v-main>
+        <h1>Conteúdo principal</h1>
+        <p>Este é o conteúdo principal da página.</p>
+        <p id="bem_vindo_usuario"></p>
+        <p>Nome: <span id="nome"></span> </p>
+        <p>Username: <span id="username"></span> </p>
+        <p>Qtd de seguidores: <span id="qtd_seguidores"></span> </p>
+        <p>Qtd de publicações: <span id="qtd_publicacoes"></span> </p>
 
-    <v-snackbar
-        v-model="snackbar"
-        :timeout="timeout"
-    >
-        {{ text }}
+        <v-form class="d-flex" ref="form" @submit.prevent="handleSubmit">
+            <v-text-field 
+                class="w-25 me-5"
+                label="Nome" 
+                variant="outlined" 
+                :rules="[v => !!v || 'Nome é obrigatório!']"
+                v-model="post.usuario"
+                required
+            ></v-text-field>
 
-        <template v-slot:actions>
-        <v-btn
-            color="red"
-            variant="text"
-            @click="snackbar = false"
+            <v-text-field 
+                class="w-50"
+                label="Imagem" 
+                variant="outlined" 
+                :rules="[v => !!v || 'Imagem é obrigatória!']"
+                v-model="post.imagem"
+                required
+            ></v-text-field>
+
+            <v-btn variant="tonal" class="mx-5" color="green" type="submit">Cadastrar</v-btn>
+            <v-btn variant="tonal" color="orange" type="reset">Limpar</v-btn>
+        </v-form>
+
+        <v-snackbar
+            v-model="snackbar"
+            :timeout="timeout"
         >
-            Fechar
-        </v-btn>
-        </template>
-    </v-snackbar>
+            {{ text }}
 
-    <div class="d-flex flex-wrap w-100">
-        <v-card 
-            v-for="(item, index) in postsList" :key="index"
-            variant="tonal" 
-            class="w-25 ma-2" 
-            :title="item.usuario"
-        >
-        
-            <v-card-text>
-                <img :width="200" :src="item.imagem" alt="usuario">
-            </v-card-text>
-        </v-card>
-    </div>
+            <template v-slot:actions>
+            <v-btn
+                color="red"
+                variant="text"
+                @click="snackbar = false"
+            >
+                Fechar
+            </v-btn>
+            </template>
+        </v-snackbar>
+
+        <div class="d-flex flex-wrap w-100">
+            <v-card 
+                v-for="(item, index) in postsList" :key="index"
+                variant="tonal" 
+                class="w-25 ma-2" 
+                :title="item.usuario"
+            >
+            
+                <v-card-text>
+                    <img :width="200" :src="item.imagem" alt="usuario">
+                </v-card-text>
+            </v-card>
+        </div>
+    </v-main>
+    </v-layout>
 </template>
 
 <script>
