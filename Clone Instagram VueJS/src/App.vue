785e3ca072e3fb60e2be15756502b533fcd17aa7
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-08-28 15:49:37 -0300

    Adição do menu de navegação

--- Clone Instagram VueJS/src/App.vue
+++ Clone Instagram VueJS/src/App.vue
@@ -1,8 +1,20 @@
 <!-- HTML -->
 <template>
+    <v-layout>
+        <v-app-bar color="blue">
+            <v-app-bar-title> Instagram</v-app-bar-title>
 
-<router-view> </router-view>
+            <v-btn icon>
+                <router-link to="/posts/novo">
+                    <v-icon color="#FFF">mdi-post</v-icon>
+                </router-link>
+            </v-btn>
+        </v-app-bar>
 
+        <v-main>
+            <router-view> </router-view>
+        </v-main>
+    </v-layout>
 </template>
 
 <!-- Informações do projeto
